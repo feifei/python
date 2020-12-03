c87a826ea44e149203fa3f658cca2709f62ba672
Feifei Xu <feifei.xu@ebc.uu.se>
2020-12-03 11:21:24 +0100

    Retrieve seq record from given seqid

---
+++ sub_fasta_by_del_seqid.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+'''
+    Remove given seqs from fasta file,
+    sub header to before |
+'''
+import argparse
+from Bio import SeqIO
+
+parser = argparse.ArgumentParser()
+parser.add_argument('infile', help="Fasta file to extract seq from")
+parser.add_argument('seqids', help="A list of geneids, with , to separate")
+args = parser.parse_args()
+
+fasta_file = args.infile
+seqids = args.seqids.split(",")
+
+with open(fasta_file, 'r') as inh:
+    for record in SeqIO.parse(inh, 'fasta'):
+        seqid = record.id.split("|")[0]
+        if seqid in seqids:
+            continue
+        else:
+            print(">%s" %seqid)
+            print(record.seq)
+        
